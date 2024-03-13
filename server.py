9078b411f5b1a30f52977bc81b4ee4791415e06a
Patryk Kurek <patryk744@op.pl>
2024-03-13 23:45:03 +0100

    feat: support to js

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_home_page():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/deal", methods=["POST"])
