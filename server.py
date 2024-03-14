6cf27fcb63ba9d24459a695859ecb023d5363d29
Wojtke7 <wojtekmarcela@interia.pl>
2024-03-14 13:03:32 +0100

    Home page html structure and styles added

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_home_page():
-    return render_template("index.html")
+    return render_template("start.html")
 
 
 @app.route("/deal", methods=["POST"])
